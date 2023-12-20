aa9f3e5f8ecc75cd1e24f773019591a47382704a
ilman30 <ilmannurhakim0@gmail.com>
2023-12-20 20:24:04 +0700

    middleware jwt auth

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\Users\UserController;
 use Illuminate\Http\Request;
@@ -20,15 +22,23 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('register', [RegisterController::class, 'create']);
+Route::post('login', [AuthController::class, 'authenticate']);
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::group(['prefix' => 'pegawai'], function () {
+        Route::get('get-data',  [PegawaiController::class, 'getDataPegawai']);
+        Route::post('save-data',  [PegawaiController::class, 'saveDataPegawai']);
+        Route::delete('delete-data',  [PegawaiController::class, 'deleteDataPegawai']);
+    });
+});
+
+
+
 Route::group(['prefix' => 'users'], function () {
     Route::get('get-data',  [UserController::class, 'getDataUser']);
     Route::post('create-data', [UserController::class, 'createDataUser']);
     Route::put('update-data', [UserController::class, 'updateDataUser']);
     Route::delete('delete-data', [UserController::class, 'deleteDataUser']);
-});
-
-Route::group(['prefix' => 'pegawai'], function () {
-    Route::get('get-data',  [PegawaiController::class, 'getDataPegawai']);
-    Route::post('save-data',  [PegawaiController::class, 'saveDataPegawai']);
-    Route::delete('delete-data',  [PegawaiController::class, 'deleteDataPegawai']);
 });
